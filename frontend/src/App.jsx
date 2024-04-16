621b768c7cc35f7ab54ccf50989da8556df34d5a
JayCox90 <jaycox@gmx.net>
2024-04-16 14:27:01 +0200

    tinkered with map setup for mapbox and env file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import ReactMapGl from "react-map-gl";
+import React, { useState } from 'react'
+import ReactMapGl from 'react-map-gl'
 import './App.css'
+import.meta.env
 
 export default function App() {
   const [viewport, setViewport] = useState({
-  latitude: 47.3769,
-  longitude: 8.5417,
-  width: "100vw",
-  height: "100vh",
-  zoom: 10
+    latitude: 47.3769,
+    longitude: 8.5417,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10,
+  })
 
-  });
+  // This is the mapbox token, defined in .env variable. Need to figure out how to "hide" in production
+  const token = import.meta.env.VITE_MAPBOX_TOKEN
 
   return (
     <div>
-     <ReactMapGl 
-     {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-     onViewportChange={viewport => {
-      setViewport(viewport);
-     }}>markers</ReactMapGl>
+      <ReactMapGl
+        {...viewport}
+        mapboxAccessToken={token}
+        // This is the move functionality
+        onMove={(evt) => setViewport(evt.viewState)}
+        // Styling need to figure out
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle="mapbox://styles/mihaels/clv1rnydt009x01qzdbpu4glb"
+      >
+        markers
+      </ReactMapGl>
     </div>
-  );
+  )
 }
-
-

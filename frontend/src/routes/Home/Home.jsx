58242a94218161ddf6211713a155b682092be483
Vladimir <v.kumov@gmail.com>
2024-04-17 13:50:47 +0200

    buttons and inputs

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,10 +1,17 @@
 import { MainContainer, SectionContainer } from '../../styles'
+import { AccentButton, SimpleButton } from '../../styles/elements'
 
 const Home = () => {
   return (
     <MainContainer>
       <SectionContainer>
         <h1>Home</h1>
+        <fotm>
+         <input placeholder='write something'></input>
+         <textarea placeholder='want to say something?'></textarea>
+         <AccentButton>click me</AccentButton>
+        </fotm>
+        <SimpleButton>And Me!</SimpleButton>
       </SectionContainer>
     </MainContainer>
   )

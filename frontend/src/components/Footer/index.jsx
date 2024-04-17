0e5574f50d113fc218379454fd437362d3a279b7
Vladimir <v.kumov@gmail.com>
2024-04-17 14:05:12 +0200

    footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { Copyright, FooterContainer, FooterCopyright, FooterMainContainer } from './styles'
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
-    
-    </footer>
-  );
+    <FooterContainer>
+      <FooterMainContainer>
+        <h3>Cycable</h3>
+      </FooterMainContainer>
+      <FooterCopyright>
+        <Copyright>Made with ❤️ in Constructor Academy. © 2024</Copyright>
+      </FooterCopyright>
+    </FooterContainer>
+  )
 }
 
-export default Footer;
+export default Footer

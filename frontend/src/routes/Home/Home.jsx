8c989bdd38a07178c364b5332bfbb0c38d0167a9
Janani <jananivenugopal23@gmail.com>
2024-04-19 17:57:38 +0200

    updated bicycleaccident route in frontend

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -2,19 +2,14 @@ import { MainContainer, SectionContainer } from '../../styles'
 import { AccentButton, SimpleButton } from '../../styles/elements/buttons'
 import { BasicForm, FormTwoColumn } from '../../styles/elements/forms'
 import { MapContainer, MapIframe } from './styles'
+import Map from '../../components/MapBox/Map/Map'
 
 const Home = () => {
   return (
     <MainContainer>
       <SectionContainer>
         <h1>Home</h1>
-      </SectionContainer>
-      <SectionContainer>
-        <FormTwoColumn>
-          <input placeholder="write something"></input>
-          <textarea placeholder="want to say something?"></textarea>
-          <AccentButton>click me</AccentButton>
-        </FormTwoColumn>
+        <Map/>
       </SectionContainer>
     </MainContainer>
   )

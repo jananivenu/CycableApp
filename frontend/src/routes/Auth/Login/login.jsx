c00a2f8b3b76d0d345e95d793c9bcdce54105c3f
Vladimir <v.kumov@gmail.com>
2024-04-19 02:23:58 +0200

    login page styles

--- frontend/src/routes/Auth/Login/login.jsx
+++ frontend/src/routes/Auth/Login/login.jsx
@@ -1,36 +1,78 @@
-import { useState } from "react";
-import * as styles from "./styles.css"
+// import { useState } from "react";
+// import * as styles from "./styles.css"
+
+// const Login = () => {
+//     const [email, setEmail] = useState("")
+//     const [password, setPassword] = useState("")
+
+//     const onSubmitHandler = (e) => {
+//         e.preventDefault()
+//     }
+
+//     return (
+//         <div className={styles.container}>
+//             <h2 className={styles.title}>LOGIN</h2>
+//             <form onSubmit={onSubmitHandler} className={styles.form}>
+//                 <input
+//                 type="text"
+//                 placeholder="email"
+//                 required
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 />
+//                 <input
+//                 type="password"
+//                 placeholder="password"
+//                 required
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 />
+//                 <button>Login</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Login
+
+import { useState } from 'react'
+import { MainContainer, NarrowSectionContainer } from '../../../styles'
+import { BasicForm } from '../../../styles/elements/forms'
+import { AccentButton } from '../../../styles/elements/buttons'
+import { StyledH2 } from '../../../styles/elements/typography'
 
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault()
-    }
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
 
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>LOGIN</h2>
-            <form onSubmit={onSubmitHandler} className={styles.form}>
-                <input
-                type="text"
-                placeholder="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                type="password"
-                placeholder="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
-                <button>Login</button>
-            </form>
-        </div>
-    )
+  return (
+    <MainContainer>
+      <NarrowSectionContainer>
+        <StyledH2>Login</StyledH2>
+        <BasicForm onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            placeholder="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <AccentButton>Login</AccentButton>
+        </BasicForm>
+      </NarrowSectionContainer>
+    </MainContainer>
+  )
 }
 
 export default Login

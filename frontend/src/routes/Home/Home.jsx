91ec08b55732a7d8cbc4a758b36535f7269f5dd6
Mihael Softic <mihael.softic@gmail.com>
2024-04-22 10:55:50 +0200

    location center

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -2,12 +2,13 @@ import { MainContainer, SectionContainer } from '../../styles'
 import { AccentButton, SimpleButton } from '../../styles/elements/buttons'
 import { BasicForm, FormTwoColumn } from '../../styles/elements/forms'
 import { MapContainer, MapIframe } from './styles'
-
+import Map from '../../components/MapBox/Map/Map'
 const Home = () => {
   return (
     <MainContainer>
       <SectionContainer>
         <h1>Home</h1>
+        <Map/>
       </SectionContainer>
       <SectionContainer>
         <FormTwoColumn>
@@ -17,6 +18,7 @@ const Home = () => {
         </FormTwoColumn>
       </SectionContainer>
     </MainContainer>
+    
   )
 }
 

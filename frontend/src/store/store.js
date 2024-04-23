4616a3fbf775d8ef54d2e0723d5d2a0a4bbc04f0
Imane Boumeshouli <boumeshouliimane@gmail.com>
2024-04-23 19:18:29 +0000

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
 import reportsReducer from './slices/reportsSlice'
+import createReportsReducer from './slices/reportCreateSlice'
 
 const store = configureStore({
     reducer: {

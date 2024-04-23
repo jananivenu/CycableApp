94c071949e79f825331324fef4a96a8401da09f0
Imane Boumeshouli <boumeshouliimane@gmail.com>
2024-04-23 17:53:14 +0200

    prev commit

--- frontend/src/routes/Reports/CreateReport/index.jsx
+++ frontend/src/routes/Reports/CreateReport/index.jsx
@@ -1,6 +1,16 @@
+import { StyledH2, StyledH3 } from '../../../styles/elements/typography'
+
 // main component to render different create_report_types
 const CreateReport = () => {
-  return <div>{/* display list of types */}</div>
+  return (
+    <div>
+      {/* display list of types */}
+      <StyledH3>Bicycle Accident</StyledH3>
+      <StyledH3>Bicycle Theft</StyledH3>
+      <StyledH3>Dangerous Locations</StyledH3>
+      <StyledH3>Legal* Violations </StyledH3>
+    </div>
+  )
 }
 
 export default CreateReport

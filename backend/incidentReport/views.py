cebd087b8bff2842d06d097cc108fa1106427a38
Janani <jananivenugopal23@gmail.com>
2024-04-26 13:02:13 +0200

    updated report by geolocation

--- backend/incidentReport/views.py
+++ backend/incidentReport/views.py
@@ -1,7 +1,8 @@
-
 from django.http import Http404
 from django.utils import timezone
-
+from django.http import JsonResponse
+from django.db.models import Func, F
+from django.db.models.functions import Cos, Sin, ACos, Radians
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
 from incidentReport.serializers import IncidentReportSerializer, SimpleIncidentReportSerializer
@@ -20,7 +21,8 @@ class ListAllIncidentReportsView(ListAPIView):
         queryset = ReportedIncidents.objects.all()
         start_date = self.request.query_params.get('start')
         end_date = self.request.query_params.get('end')
-
+        latitude = self.request.query_params.get('latitude')
+        longitude = self.request.query_params.get('longitude')
         if start_date and end_date:
             start_datetime = timezone.make_aware(timezone.datetime.strptime(start_date, '%Y-%m-%d'))
             end_datetime = timezone.make_aware(timezone.datetime.strptime(end_date, '%Y-%m-%d'))
@@ -35,6 +37,20 @@ class ListAllIncidentReportsView(ListAPIView):
             #  to include the end of the day
             end_datetime = end_datetime.replace(hour=23, minute=59, second=59, microsecond=999999)
             queryset = queryset.filter(custom_date__lte=end_datetime)
+        elif latitude and longitude:
+            latitude = float(self.request.GET.get('latitude'))
+            longitude = float(self.request.GET.get('longitude'))
+            radius = float(self.request.GET.get('radius', 10))
+            distance_expression = (
+                    ACos(
+                        Cos(Radians(latitude)) * Cos(Radians(F('latitude'))) *
+                        Cos(Radians(F('longitude')) - Radians(longitude)) +
+                        Sin(Radians(latitude)) * Sin(Radians(F('latitude')))
+                    ) * 6371
+            )
+            queryset = ReportedIncidents.objects.annotate(distance=distance_expression).filter(distance__lte=radius)
+        else:
+            raise Http404('Invalid query parameters')
 
         return queryset
 

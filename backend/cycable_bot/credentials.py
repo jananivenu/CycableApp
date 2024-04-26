96c1fae50bb150ff17212620a2b6b4a3deb62ce4
Imane Boumeshouli <boumeshouliimane@gmail.com>
2024-04-26 14:08:25 +0200

    tg bot

---
+++ backend/cycable_bot/credentials.py
@@ -0,0 +1,3 @@
+# Do not remove these 2 lines:
+BOT_TOKEN = '6944175385:AAFH0R0zunG-39j7HBbwyGwFzxR-vzVES0g'  # You should consider using env variables or a secret manager for this.
+APP_NAME = 'cycable_bot'

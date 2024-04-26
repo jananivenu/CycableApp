bf0ee7de2cc09680e3a55df65891dbb4438b6c93
Janani <jananivenugopal23@gmail.com>
2024-04-26 15:15:30 +0200

    pushing Dockerfile

--- backend/incidentReport/urls.py
+++ backend/incidentReport/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('type/<str:incident_type>/', ListIncidentReportsByTypeView.as_view()),
     path('user/<int:user_id>/', ListIncidentReportsByUserView.as_view()),
     path('<int:pk>/', ReadUpdateDeleteIncidentReportView.as_view()),
-    path('latlong/',ListAllIncidentReportsByLatLongView.as_view())
+    path('latlong/', ListAllIncidentReportsByLatLongView.as_view())
 ]

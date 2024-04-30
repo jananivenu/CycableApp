39300e149f4b94a9f8312d23f825347f62ad3aec
Imane Boumeshouli <boumeshouliimane@gmail.com>
2024-04-30 14:05:02 +0200

    report pages edited, including the op up thanks msg, fixed error to report near miss

---
+++ frontend/src/routes/Reports/CreateReport/Bicycle_accident/IntroYesNo.jsx
@@ -0,0 +1,7 @@
+import { StyledH3 } from '../../../../styles/elements/typography'
+
+function IntroYesNo() {
+  return <StyledH3>Was the police called to document the accident?</StyledH3>
+}
+
+export default IntroYesNo
